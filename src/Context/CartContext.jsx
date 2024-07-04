b688bf6d50fdd9ab52775a50c8d7b32fcd5ff49d
PriyankagithubS <priyankashanmugasundaramr@gmail.com>
2024-07-04 20:19:06 +0530

    calculation change

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -30,7 +30,7 @@ export const CartProvider = ({ children }) => {
     const decreaseQuantity = (id) => {
         setCart((prevCart) =>
             prevCart.map((item) =>
-                item.id === id && item.quantity > 1
+                item.id === id && item.quantity > 0
                     ? { ...item, quantity: item.quantity - 1 }
                     : item
             )
